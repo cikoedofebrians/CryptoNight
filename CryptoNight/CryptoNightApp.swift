a47fa9c2a3c54abd58ebae230da2a5b1b47fe15f
cikoedofebrian <ciko.edo.febrians@gmail.com>
2023-10-12 17:37:43 +0700

    Initial Commit

---
+++ CryptoNight/CryptoNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoNightApp.swift
+//  CryptoNight
+//
+//  Created by Ciko Edo Febrian on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
